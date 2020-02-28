421c9fdb3c00863086b9e6c417aedbc78ad5dbab
mygit <jingyin2080@163.com>
2020-02-28 14:34:14 +0800

    修改文件666

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        //测试123
+        //测试666
         System.out.println("Hello java");
         System.out.println("Hello World!");
     }
